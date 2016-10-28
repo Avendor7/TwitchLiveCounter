c7ac8b645abfab1b5105bf721946d7da63c8f064
Stephen <steve.wiggins7@gmail.com>
2016-10-28 03:20:53 -0400

    Twitch Get request is working

--- TwitchLiveCounter/TwitchLiveCounter/TwitchLiveCounter.Designer.cs
+++ TwitchLiveCounter/TwitchLiveCounter/TwitchLiveCounter.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // TwitchLiveCounter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(458, 340);
+            this.Name = "TwitchLiveCounter";
+            this.Text = "Twitch Live Counter";
+            this.Load += new System.EventHandler(this.TwitchLiveCounter_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- TwitchLiveCounter/TwitchLiveCounter/TwitchLiveCounter.cs
+++ TwitchLiveCounter/TwitchLiveCounter/TwitchLiveCounter.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RestSharp;
 
 namespace TwitchLiveCounter
 {
@@ -16,5 +17,29 @@ namespace TwitchLiveCounter
         {
             InitializeComponent();
         }
-    }
+
+        private void TwitchLiveCounter_Load(object sender, EventArgs e)
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri("https://api.twitch.tv/kraken/streams/");
+           
+
+            var request = new RestRequest();
+            request.Method = Method.GET;
+            request.Resource = "KatGunn";
+            request.AddHeader("Client-ID", "dsf248t4b6aririduqsh94h9ypzrb0i");
+            request.AddHeader("accept", "application/vnd.twitchtv.v3+json");
+            request.RequestFormat = DataFormat.Json;
+
+            IRestResponse response = client.Execute(request);
+           
+            Console.Write(response.Content);
+
+
+        }
+
+
+       
+
+        }
 }
