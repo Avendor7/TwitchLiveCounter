2108f8921e6c2280f507874acbfad29f70d96c90
Stephen <steve.wiggins7@gmail.com>
2016-11-04 05:13:34 -0400

    code now grabs followed data from API, working on live status

--- TwitchLiveCounter/TwitchLiveCounter/TwitchLiveCounter.Designer.cs
+++ TwitchLiveCounter/TwitchLiveCounter/TwitchLiveCounter.Designer.cs
@@ -28,21 +28,204 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.getTwitchUsers = new System.Windows.Forms.Button();
+            this.twitchUsernameTextBox = new System.Windows.Forms.TextBox();
+            this.followerList = new System.Windows.Forms.ListView();
+            this.Usernames = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.updateNowButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.totalFollowersLabel = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // getTwitchUsers
+            // 
+            this.getTwitchUsers.Location = new System.Drawing.Point(134, 55);
+            this.getTwitchUsers.Name = "getTwitchUsers";
+            this.getTwitchUsers.Size = new System.Drawing.Size(107, 23);
+            this.getTwitchUsers.TabIndex = 0;
+            this.getTwitchUsers.Text = "Get Twitch Users";
+            this.getTwitchUsers.UseVisualStyleBackColor = true;
+            this.getTwitchUsers.Click += new System.EventHandler(this.getTwitchUsers_Click);
+            // 
+            // twitchUsernameTextBox
+            // 
+            this.twitchUsernameTextBox.Location = new System.Drawing.Point(28, 58);
+            this.twitchUsernameTextBox.Name = "twitchUsernameTextBox";
+            this.twitchUsernameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.twitchUsernameTextBox.TabIndex = 1;
+            this.twitchUsernameTextBox.TextChanged += new System.EventHandler(this.twitchUsernameTextBox_TextChanged);
+            // 
+            // followerList
+            // 
+            this.followerList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Usernames});
+            this.followerList.FullRowSelect = true;
+            this.followerList.GridLines = true;
+            this.followerList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.followerList.Location = new System.Drawing.Point(28, 109);
+            this.followerList.Name = "followerList";
+            this.followerList.Size = new System.Drawing.Size(213, 130);
+            this.followerList.TabIndex = 2;
+            this.followerList.UseCompatibleStateImageBehavior = false;
+            this.followerList.View = System.Windows.Forms.View.Details;
+            // 
+            // updateNowButton
+            // 
+            this.updateNowButton.Location = new System.Drawing.Point(28, 295);
+            this.updateNowButton.Name = "updateNowButton";
+            this.updateNowButton.Size = new System.Drawing.Size(213, 23);
+            this.updateNowButton.TabIndex = 3;
+            this.updateNowButton.Text = "Update Now";
+            this.updateNowButton.UseVisualStyleBackColor = true;
+            this.updateNowButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter Twitch Username";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "List of Twitch Users to Poll for Live status";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(28, 260);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(131, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Update Interval (mins)";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(275, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem1});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.aboutToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem1.Text = "About";
+            // 
+            // totalFollowersLabel
+            // 
+            this.totalFollowersLabel.AutoSize = true;
+            this.totalFollowersLabel.Location = new System.Drawing.Point(28, 242);
+            this.totalFollowersLabel.Name = "totalFollowersLabel";
+            this.totalFollowersLabel.Size = new System.Drawing.Size(84, 13);
+            this.totalFollowersLabel.TabIndex = 9;
+            this.totalFollowersLabel.Text = "Total Followers: ";
             // 
             // TwitchLiveCounter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(458, 340);
+            this.ClientSize = new System.Drawing.Size(275, 325);
+            this.Controls.Add(this.totalFollowersLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.updateNowButton);
+            this.Controls.Add(this.followerList);
+            this.Controls.Add(this.twitchUsernameTextBox);
+            this.Controls.Add(this.getTwitchUsers);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "TwitchLiveCounter";
             this.Text = "Twitch Live Counter";
             this.Load += new System.EventHandler(this.TwitchLiveCounter_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button getTwitchUsers;
+        private System.Windows.Forms.TextBox twitchUsernameTextBox;
+        private System.Windows.Forms.ListView followerList;
+        private System.Windows.Forms.Button updateNowButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+        private System.Windows.Forms.ColumnHeader Usernames;
+        private System.Windows.Forms.Label totalFollowersLabel;
     }
 }
 
--- TwitchLiveCounter/TwitchLiveCounter/TwitchLiveCounter.cs
+++ TwitchLiveCounter/TwitchLiveCounter/TwitchLiveCounter.cs
@@ -14,55 +14,162 @@ namespace TwitchLiveCounter
 {
     public partial class TwitchLiveCounter : Form
     {
-        public TwitchLiveCounter()
-        {
+
+        private string twitchUsername = "Avendor7";
+        private string usernameString = "";
+        
+        public TwitchLiveCounter(){
             InitializeComponent();
+            
         }
 
-        private void TwitchLiveCounter_Load(object sender, EventArgs e)
-        {
+        private void TwitchLiveCounter_Load(object sender, EventArgs e) {
+            
+
+        }
+        
+        private void getLiveStatus() {
+
             var client = new RestClient();
             client.BaseUrl = new Uri("https://api.twitch.tv/kraken/streams/");
 
-            string[] streamer = new string[3] { "katgunn", "esl_overwatch", "dexteritybonus" };
+            var request = new RestRequest();
+            request.Method = Method.GET;
+           // request.Resource = streamer[i];
+            request.AddHeader("Client-ID", "dsf248t4b6aririduqsh94h9ypzrb0i");
+            request.AddHeader("accept", "application/vnd.twitchtv.v3+json");
+            request.RequestFormat = DataFormat.Json;
 
-            for (int i =0; i<=2; i++) {
-                var request = new RestRequest();
-                request.Method = Method.GET;
-                request.Resource = streamer[i];
-                request.AddHeader("Client-ID", "dsf248t4b6aririduqsh94h9ypzrb0i");
-                request.AddHeader("accept", "application/vnd.twitchtv.v3+json");
-                request.RequestFormat = DataFormat.Json;
+            IRestResponse response = client.Execute(request);
 
-                IRestResponse response = client.Execute(request);
+            // Console.WriteLine(response.Content);
 
-                // Console.WriteLine(response.Content);
+            dynamic results = JsonConvert.DeserializeObject<dynamic>(response.Content);
 
-                dynamic results = JsonConvert.DeserializeObject<dynamic>(response.Content);
-                var stream = results.stream;
-
-                if (stream != null) {
-                    Console.WriteLine(streamer[i] + " is Live");
-                }
-                else {
-                    Console.WriteLine(streamer[i] + " is NOT Live");
-                }
-
-            }
             
 
-            //Console.WriteLine(stream);
-            /*
-            if (!string.IsNullOrEmpty(results.stream))
-            {
-                Console.WriteLine("Streamer is Live");
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            getLiveStatus();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e) {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e) {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void getTwitchUsers_Click(object sender, EventArgs e) {
+
+            //followerList.Clear();
+            Usernames.Width = followerList.Size.Width - 21;
+
+            twitchUsername = twitchUsernameTextBox.Text;
+
+            var client = new RestClient();
+            client.BaseUrl = new Uri("https://api.twitch.tv/kraken/users/");
+
+            var request = new RestRequest();
+            request.Method = Method.GET;
+            request.Resource = twitchUsername + "/follows/channels?limit=100";
+            request.AddHeader("Client-ID", "dsf248t4b6aririduqsh94h9ypzrb0i");
+            request.AddHeader("accept", "application/vnd.twitchtv.v3+json");
+            request.RequestFormat = DataFormat.Json;
+          
+            IRestResponse response = client.Execute(request);
+            
+            var rootObj = JsonConvert.DeserializeObject<Rootobject>(response.Content);
+
+            foreach (var row in rootObj.follows) {
+                Console.WriteLine(row.channel.display_name);
+                followerList.Items.Add(row.channel.display_name);
+                usernameString += row.channel.display_name + ",";
             }
-            */
-
+            totalFollowersLabel.Text = "Total Followers: " + rootObj._total.ToString();
+            Console.WriteLine(rootObj._total);
+            
         }
 
-
-       
+        private void twitchUsernameTextBox_TextChanged(object sender, EventArgs e) {
 
         }
+    }
+
+    //twitch classes
+
+    public class Rootobject {
+        public Follow[] follows { get; set; }
+        public int _total { get; set; }
+        public _Links _links { get; set; }
+    }
+
+    public class _Links {
+        public string self { get; set; }
+        public string next { get; set; }
+    }
+
+    public class Follow {
+        public DateTime created_at { get; set; }
+        public _Links1 _links { get; set; }
+        public bool notifications { get; set; }
+        public Channel channel { get; set; }
+    }
+
+    public class _Links1 {
+        public string self { get; set; }
+    }
+
+    public class Channel {
+        public _Links2 _links { get; set; }
+        public object background { get; set; }
+        public object banner { get; set; }
+        public string broadcaster_language { get; set; }
+        public string display_name { get; set; }
+        public string game { get; set; }
+        public string logo { get; set; }
+        //public bool mature { get; set; }
+        public string status { get; set; }
+        public bool partner { get; set; }
+        public string url { get; set; }
+        public string video_banner { get; set; }
+        public int _id { get; set; }
+        public string name { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+        public object delay { get; set; }
+        public int followers { get; set; }
+        public string profile_banner { get; set; }
+        public string profile_banner_background_color { get; set; }
+        public int views { get; set; }
+        public string language { get; set; }
+    }
+
+    public class _Links2 {
+        public string self { get; set; }
+        public string follows { get; set; }
+        public string commercial { get; set; }
+        public string stream_key { get; set; }
+        public string chat { get; set; }
+        public string subscriptions { get; set; }
+        public string editors { get; set; }
+        public string videos { get; set; }
+        public string teams { get; set; }
+    }
+
+
 }
